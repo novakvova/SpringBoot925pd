02d45fe8004c1aeee1ce36d9b1ba029772baaa75
novakvova <novakvova@gmail.com>
2021-07-12 17:53:11 +0300

    Begin working spring boot

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -1,14 +1,19 @@
 package com.example.springboot;
 
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMapping;
 
-@RestController
+
+//import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+
+//@RestController
+@Controller
 public class HelloController {
 
 	@RequestMapping("/")
 	public String index() {
-		return "Greetings from Spring Boot!";
+		return "index";
 	}
 
 }
