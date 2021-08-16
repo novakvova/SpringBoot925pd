d1e77d4c08b070da68d0050576f02eb5857ef92d
novakvova <novakvova@gmail.com>
2021-08-16 16:13:26 +0300

    Add auth in database

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -4,6 +4,7 @@ package com.example.springboot;
 
 //import org.springframework.stereotype.Controller;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 //import org.springframework.web.bind.annotation.RestController;
 
@@ -14,6 +15,11 @@ public class HelloController {
 	@RequestMapping("/")
 	public String index() {
 		return "index";
+	}
+
+	@GetMapping("/login")
+	public String login() {
+		return "login";
 	}
 
 }
